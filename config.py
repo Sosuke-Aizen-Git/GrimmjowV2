a5cc95fe82960d47c2c9e53657fb47bd262c51a9
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-02-28 04:06:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,73 +1,52 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
-
-
-
+from database.db_handler import get_force_sub_channel
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7295673972:AAGz3L2ArpAd1YLx5frjGrl2oU2_oPL7fzY")
 API_ID = int(os.environ.get("API_ID", "20420188"))
 API_HASH = os.environ.get("API_HASH", "ac372bd945f5773afe0b714c6429039b")
-
-
-OWNER_ID = int(os.environ.get("OWNER_ID", "5743248220"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1993048420"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://itsintrovert07:sanemibot@cluster0.zd1nrbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "sahil")
 
-
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002121888464"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002394309960"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002004600395"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002032097452"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002127951847"))
 
+FORCE_SUB_CHANNEL_1 = get_force_sub_channel(1)
+FORCE_SUB_CHANNEL_2 = get_force_sub_channel(2)
+FORCE_SUB_CHANNEL_3 = get_force_sub_channel(3)
+FORCE_SUB_CHANNEL_4 = get_force_sub_channel(4)
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "3600")) # auto delete in seconds
-
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "3600"))  # auto delete in seconds
 
 PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-
-
 try:
-    ADMINS=[6848088376]
+    ADMINS = [6848088376]
     for x in (os.environ.get("ADMINS", "1993048420 5743248220 1214348787 6297086120").split()):
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 
-
-
-
-
-
-
+try:
+    SUDO_USERS = []
+    for x in (os.environ.get("SUDO_USERS", "1993048420 5743248220 1214348787").split()):
+        SUDO_USERS.append(int(x))
+except ValueError:
+    raise Exception("Your SUDO_USERS list does not contain valid integers.")
 
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
 DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
-
-
-
-
-
-
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only Work For - @Animes_Station !"
 
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link\n\nMain channel @Animes_Station.")
-
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
-
-
-
-
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6848088376)
@@ -89,14 +68,8 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   
-
-
-
-
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
