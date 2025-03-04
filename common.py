565d76b6c691996abde6622b668af4db0fe7f816
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-03-05 03:57:22 +0530

    Update common.py

--- common.py
+++ common.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, __version__
 from pyrogram.enums import ParseMode
-
+from config.py import BOT_TOKEN, API_ID, API_HASH, TG_BOT_WORKERS
 class Bot(Client):
     def __init__(self, api_hash, api_id, bot_token, tg_bot_workers, logger):
         super().__init__(
             name="Bot",
-            api_hash=api_hash,
-            api_id=api_id,
+            api_hash=API_HASH,
+            api_id=API_ID,
             plugins={"root": "plugins"},
-            workers=tg_bot_workers,
-            bot_token=bot_token
+            workers=TG_BOT_WORKERS,
+            bot_token=BOT_TOKEN
         )
         self.LOGGER = logger
 
