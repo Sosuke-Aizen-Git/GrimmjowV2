c12c571a1ed47f045abdcd6300911e286e2be398
NotCodingbot <notcodingbot@gmail.com>
2025-03-07 08:50:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,7 @@ from helper_func import subscribed, encode, decode, get_messages
 from database.db_handler import get_force_sub_channel, refresh_db_handler
 from database.database import add_user, del_user, full_userbase, present_user
 from plugins.cbb import check_force_sub, refresh_force_sub_channels, cache_invite_links   # Import the check_force_sub function
+from plugins.refresh import 
 import pymongo
 import random  # Import random to select a random photo
 
