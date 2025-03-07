854f75da28ec9aa2e43be9ab659fc60255d3aa91
NotCodingbot <notcodingbot@gmail.com>
2025-03-07 08:51:19 +0530

    Update refresh.py

--- plugins/refresh.py
+++ plugins/refresh.py
@@ -1,6 +1,13 @@
 from database.db_handler import get_force_sub_channel, refresh_db_handler
 from database.database import add_user, del_user, full_userbase, present_user
+from config import DB_URL, DB_NAME
 
+async def refresh_database():
+    global dbclient, database, user_data
+    dbclient = pymongo.MongoClient(DB_URL)
+    database = dbclient[DB_NAME]
+    user_data = database['users']
+    return
 
 @Bot.on_message(filters.command('refresh') & filters.private & filters.user([OWNER_ID] + SUDO_USERS))
 @Bot.on_message(filters.command('refresh') & filters.group & filters.user([OWNER_ID] + SUDO_USERS))
