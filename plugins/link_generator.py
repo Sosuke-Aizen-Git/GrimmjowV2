2cfb9e80c2416e041f1664ee474c30d581ca8485
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-03-07 19:04:32 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -42,7 +42,7 @@ async def batch(client: Client, message: Message):
 
 
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@Bot.on_message(filters.private & filters.user(ADMINS + (get_admins())) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
