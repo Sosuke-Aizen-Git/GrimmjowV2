694a1396d0125164ed2017bdec9f5d082df10d76
NotCodingbot <notcodingbot@gmail.com>
2025-03-07 23:12:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -277,9 +277,7 @@ async def delete_files(messages, client, k):
     await message.reply_text(admin_text, disable_web_page_preview=True, reply_markup=reply_markup)
 
 
-SUDO_ADMIN = [SUDO_USERS + get_admins()]
-
-@Client.on_message(filters.command("admins") & filters.user(SUDO_ADMIN))
+@Client.on_message(filters.command("admins") & filters.user([SUDO_USERS] + get_admins()))
 async def list_admins(client, message):
     unique_admins = list(set(SUDO_USERS + get_admins()))  # Remove duplicate IDs
     admins_list = []
