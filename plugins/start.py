32224970e5f4ebf5ba7ffea478a3b85b9ff71c4e
NotCodingbot <notcodingbot@gmail.com>
2025-03-08 18:06:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,7 @@
 import os, asyncio, humanize
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ParseMode
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, ChatAdminRequired
 from bot import Bot
 from config import ADMINS, OWNER_ID, SUDO_USERS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, FILE_AUTO_DELETE, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4, DB_URL, DB_NAME
@@ -16,7 +16,6 @@ import random  # Import random to select a random photo
 
 file_auto_delete = humanize.naturaldelta(get_auto_delete_time())
 
-# List of photo URLs
 photos = [
     "https://litter.catbox.moe/8ngis1.jpg",
     "https://litter.catbox.moe/21bhag.jpg",
@@ -25,7 +24,7 @@ photos = [
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 @Bot.on_message(filters.command('start') & filters.group & subscribed)
-async def start_command(client: Client, message: Message):
+async def start_command(client: Bot, message: Message):
     id = message.from_user.id
     if not await present_user(id):
         try:
@@ -69,7 +68,7 @@ async def start_command(client: Client, message: Message):
             return
         await temp_msg.delete()
 
-        madflix_msgs = []  # List to keep track of sent messages
+        madflix_msgs = []
 
         for msg in messages:
             if bool(CUSTOM_CAPTION) & bool(msg.document):
@@ -129,7 +128,7 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 @Bot.on_message(filters.command('start') & filters.group)
-async def not_joined(client: Client, message: Message):
+async def not_joined(client: Bot, message: Message):
     # Send "Please Wait..." message
     temp_msg = await message.reply("Please Wait...")
 
@@ -184,213 +183,6 @@ async def not_joined(client: Client, message: Message):
     # User has joined all channels, proceed with start command
     await start_command(client, message)
 
-@Bot.on_message(filters.command('users') & filters.private)
-@Bot.on_message(filters.command('users') & filters.group)
-async def get_users(client: Bot, message: Message):
-    if message.from_user.id in get_admins() + SUDO_USERS:
-        msg = await client.send_message(chat_id=message.chat.id, text="Processing...")
-        users = await full_userbase()
-        await msg.edit(f"{len(users)} Users Are Using This Bot")
-    else:
-        await message.reply_text("You are not an authorized user!")
-
-
-@Bot.on_message(filters.private & filters.command('broadcast'))
-async def send_text(client: Bot, message: Message):
-    if message.from_user.id in get_admins() + SUDO_USERS:
-        if message.reply_to_message:
-            query = await full_userbase()
-            broadcast_msg = message.reply_to_message
-            total = 0
-            successful = 0
-            blocked = 0
-            deleted = 0
-            unsuccessful = 0
-
-            pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
-            for chat_id in query:
-                try:
-                    await broadcast_msg.copy(chat_id)
-                    successful += 1
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-                    await broadcast_msg.copy(chat_id)
-                    successful += 1
-                except UserIsBlocked:
-                    await del_user(chat_id)
-                    blocked += 1
-                except InputUserDeactivated:
-                    await del_user(chat_id)
-                    deleted += 1
-                except:
-                    unsuccessful += 1
-                    pass
-                total += 1
-
-            status = f"""<b><u>Broadcast Completed</u></b>
-
-    <b>Total Users :</b> <code>{total}</code>
-    <b>Successful :</b> <code>{successful}</code>
-    <b>Blocked Users :</b> <code>{blocked}</code>
-    <b>Deleted Accounts :</b> <code>{deleted}</code>
-    <b>Unsuccessful :</b> <code>{unsuccessful}</code>"""
-
-            return await pls_wait.edit(status)
-
-        else:
-            msg = await message.reply("Use This Command As A Reply To Any Telegram Message With Out Any Spaces.")
-            await asyncio.sleep(8)
-            await msg.delete()
-    else:
-        await message.reply_text("You are not an authorized user!")
-
-@Client.on_message(filters.command("fsubs"))
-async def force_subs(client, message):
-    if message.from_user.id in get_admins() + SUDO_USERS:
-        channels = [get_force_sub_channel(1), get_force_sub_channel(2), get_force_sub_channel(3), get_force_sub_channel(4)]
-
-        buttons = []
-        for channel in channels:
-            if channel and str(channel).startswith("-100"):  # Ensure it's a valid channel ID
-                try:
-                    chat = await client.get_chat(chat_id=channel)  # Fetch channel details
-                    invite_link = await client.create_chat_invite_link(chat_id=channel)
-                    buttons.append([InlineKeyboardButton(chat.title, url=invite_link.invite_link)])  # Use channel name
-                except Exception as e:
-                    print(f"Error generating invite link for {channel}: {e}")
-
-        if not buttons:
-            return await message.reply_text("No valid force subscription channels found!")
-
-        # Add close button to the reply markup
-        buttons.append([InlineKeyboardButton("🔒 Close", callback_data="close")])
-
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text("Here is the list of force subscription channels:", reply_markup=reply_markup)
-    else:
-        await message.reply_text("You are not an authorized user!")
-# Function to handle file deletion
-async def delete_files(messages, client, k):
-    await asyncio.sleep(get_auto_delete_time())  # Wait for the duration specified in config.py
-    for msg in messages:
-        if msg is not None:  # Ensure msg is not None
-            try:
-                await client.delete_messages(chat_id=msg.chat.id, message_ids=[msg.id])
-            except Exception as e:
-                print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
-    await k.edit_text("Your Video / File Is Successfully Deleted ✅")
-
-    await message.reply_text(admin_text, disable_web_page_preview=True, reply_markup=reply_markup)
-
-
-@Client.on_message(filters.command("admins"))
-async def list_admins(client, message):
-    if message.from_user.id in get_admins() + SUDO_USERS:
-
-        unique_admins = list(set(SUDO_USERS + get_admins()))  # Remove duplicate IDs
-        admins_list = []
-
-        for index, admin_id in enumerate(unique_admins, start=1):
-            try:
-                user = await client.get_users(admin_id)  # Fetch user details
-                admin_name = f"[{user.first_name}]( tg://openmessage?user_id={admin_id} )"
-            except Exception:
-                admin_name = f"`{admin_id}` (Bot not started)"  # If user data not found
-
-            admins_list.append(f"{index}. {admin_name} (`<code>{admin_id}</code>`)")
-    
-        admin_text = "👮‍♂️ Here is the list of bot admins:\n\n" + "\n".join(admins_list)
-
-        # Add close button to the reply markup
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔒 Close", callback_data="close")]])
-
-        await message.reply_text(admin_text, disable_web_page_preview=True, reply_markup=reply_markup)
-    else:
-        await message.reply_text("You are not the authorized user!")
-
-
-@Client.on_message(filters.private & filters.command("fpbroadcast"))
-async def forward_broadcast(client, message):
-    if message.from_user.id in get_admins() + SUDO_USERS:
-        if not message.reply_to_message:
-            return await message.reply("❌ Use this command as a reply to the message you want to forward.")
-
-        query = await full_userbase()
-        forward_msg = message.reply_to_message
-        total, successful, blocked, deleted, unsuccessful = len(query), 0, 0, 0, 0
-
-        pls_wait = await message.reply("<i>Forwarding message to all users... This may take some time.</i>")
-
-        for chat_id in query:
-            try:
-                sent_msg = await forward_msg.forward(chat_id)
-                successful += 1
-
-                # Try to pin the message
-            
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                sent_msg = await forward_msg.forward(chat_id)
-                successful += 1
-                
-                    
-            except UserIsBlocked:
-                await del_user(chat_id)
-                blocked += 1
-            except InputUserDeactivated:
-                await del_user(chat_id)
-                deleted += 1
-            except Exception as e:
-                print(f"Error sending to {chat_id}: {e}")
-                unsuccessful += 1
-                continue
-
-        status = f"""<b><u>✅ Forward Broadcast Completed</u></b>
-
-    <b>Total Users :</b> <code>{total}</code>
-    <b>Successful :</b> <code>{successful}</code>
-    <b>Blocked Users :</b> <code>{blocked}</code>
-    <b>Deleted Accounts :</b> <code>{deleted}</code>
-    <b>Unsuccessful :</b> <code>{unsuccessful}</code>"""
-
-        return await pls_wait.edit(status)
-    else:
-        await message.reply_text("You are not the authorized user!")
-
-@Bot.on_message(filters.command('id') & filters.private)
-@Bot.on_message(filters.command('id') & filters.group)
-async def get_id(client: Bot, message: Message):
-    if message.reply_to_message:
-        user = message.reply_to_message.from_user
-        user_id = user.id
-        user_name = user.first_name
-    else:
-        parts = message.text.split(maxsplit=1)
-        if len(parts) == 1:
-            user = message.from_user
-            user_id = user.id
-            user_name = user.first_name
-        else:
-            username = parts[1].strip()
-            try:
-                user = await client.get_users(username)
-                user_id = user.id
-                user_name = user.first_name
-            except Exception as e:
-                await message.reply_text(f"User {username} not found.\nError: {e}")
-                return
-
-    await message.reply_text(f"User {user_name}'s ID: <code>{user_id}</code>", quote=True)
-
-# Add this import statement at the top if not already present
-from config import FILE_AUTO_DELETE
-
-
-# Add the following function in `start.py` to handle the /autodel command
-@Bot.on_message(filters.command('autodel') & filters.private)
-@Bot.on_message(filters.command('autodel') & filters.group)
-async def autodel_command(client: Client, message: Message):
-    await message.reply_text(f"The current auto delete timer is set to {(get_auto_delete_time())} seconds.")
 
 
 # Jishu Developer 
