652507b8d522f99ddfca79057fe45f1bc755d63d
NotCodingbot <notcodingbot@gmail.com>
2025-03-09 04:18:00 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -3,21 +3,32 @@ from pyrogram.types import Message
 from pyrogram import filters
 from datetime import datetime
 from helper_func import get_readable_time
-from config import ADMINS, BOT_STATS_TEXT
+from config import ADMINS
+from database.database import full_userbase
+import time
 
+async def get_ping(bot):
+    start = time.time()
+    await bot.get_me()
+    end = time.time()
+    ping = (end - start) * 1000  # Convert to milliseconds
+    return round(ping, 3)  # Round to 3 decimal places
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
-    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+    ping = await get_ping(bot)
+    total_users_count = len(await full_userbase())
 
-
-
-
-
-
+    status_message = (
+        "Bot Status\n\n"
+        f"⏳ Bot Uptime: {time}\n"
+        f"⚡️ Current Ping: {ping} ms\n"
+        f"👤 Total Users: {total_users_count}"
+    )
+    await message.reply(status_message)
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
