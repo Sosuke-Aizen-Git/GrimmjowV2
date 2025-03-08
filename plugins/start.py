33a2adc88f5e87b4ff0dac9138b0ab96767e1362
NotCodingbot <notcodingbot@gmail.com>
2025-03-09 01:58:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -246,6 +246,61 @@ async def send_text(client: Bot, message: Message):
     else:
         await message.reply_text("You are not an authorized user!")
 
+@Bot.on_message(filters.private & filters.command('pbroadcast'))
+async def send_text(client: Bot, message: Message):
+    if message.from_user.id in get_admins() + SUDO_USERS:
+        if message.reply_to_message:
+            query = await full_userbase()
+            broadcast_msg = message.reply_to_message
+            total = 0
+            successful = 0
+            blocked = 0
+            deleted = 0
+            unsuccessful = 0
+
+            pls_wait = await message.reply("<i>PBroadcasting Message.. This will Take Some Time</i>")
+            for chat_id in query:
+                try:
+                    await broadcast_msg.copy(chat_id)
+                    successful += 1
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    await broadcast_msg.copy(chat_id)
+                    successful += 1
+                    try:
+                        await client.pin_chat_message(chat_id, sent_msg.id)
+                    except ChatAdminRequired:
+                        print(f"Cannot pin message in {chat_id}, bot is not an admin.")
+            
+                except UserIsBlocked:
+                    await del_user(chat_id)
+                    blocked += 1
+                except InputUserDeactivated:
+                    await del_user(chat_id)
+                    deleted += 1
+                except:
+                    unsuccessful += 1
+                    pass
+                total += 1
+
+            status = f"""<b><u>Broadcast Completed</u></b>
+
+    <b>Total Users :</b> <code>{total}</code>
+    <b>Successful :</b> <code>{successful}</code>
+    <b>Blocked Users :</b> <code>{blocked}</code>
+    <b>Deleted Accounts :</b> <code>{deleted}</code>
+    <b>Unsuccessful :</b> <code>{unsuccessful}</code>"""
+
+            return await pls_wait.edit(status)
+
+        else:
+            msg = await message.reply("Use This Command As A Reply To Any Telegram Message With Out Any Spaces.")
+            await asyncio.sleep(8)
+            await msg.delete()
+    else:
+        await message.reply_text("You are not an authorized user!")
+
+
 @Client.on_message(filters.command("fsubs"))
 async def force_subs(client, message):
     temp_mssg = await message.reply("Ruk ek sec...")
