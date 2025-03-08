5ac05dae2332009fbe92029b0fa6fcb1f2019ed7
NotCodingbot <notcodingbot@gmail.com>
2025-03-09 03:39:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
