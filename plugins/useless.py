c0f5be9610f2961ad29d418207f09c3d6a985e36
NotCodingbot <notcodingbot@gmail.com>
2025-03-09 00:01:22 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,7 +1,6 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
@@ -15,11 +14,6 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-
-@Bot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
-    if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
 
 
 
