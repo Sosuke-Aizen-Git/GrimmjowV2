d848cb55e47c98298fd5545e8f3612339f07a0ed
NotCodingbot <notcodingbot@gmail.com>
2025-03-10 01:55:02 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -21,51 +21,23 @@ async def get_ping(bot):
 
 @Bot.on_message(filters.command('stats'))
 async def stats(bot: Bot, message: Message):
-    if message.chat.type == "private":
-        if message.from_user.id in get_admins() + SUDO_USERS:
-            sticker = await message.reply_sticker("CAACAgUAAxkBAAEG8_xnzbAQQOfHqMQrzWcOHBvU78EiRgAC_hMAAqiR-FZkMEjJt_CizDYE")
+    if message.from_user.id in get_admins() + SUDO_USERS:
+        sticker=await message.reply_sticker("CAACAgUAAxkBAAEG8_xnzbAQQOfHqMQrzWcOHBvU78EiRgAC_hMAAqiR-FZkMEjJt_CizDYE")
+        now = datetime.now()
+        delta = now - bot.uptime
+        time = get_readable_time(delta.seconds)
+        ping = await get_ping(bot)
+        total_users_count = len(await full_userbase())
 
-            now = datetime.now()
-            delta = now - bot.uptime
-            time = get_readable_time(delta.seconds)
-            ping = await get_ping(bot)
-            total_users_count = len(await full_userbase())
+        status_message = f'<pre language="@RedHoodXbot Bot Status:">⏳ Bot Uptime: {time}\n⚡ Current Ping: {ping} ms\n👤 Total Users: {total_users_count}</pre>'
 
-            status_message = f'<pre language="@RedHoodXbot Bot Status:">⏳ Bot Uptime: {time}\n⚡ Current Ping: {ping} ms\n👤 Total Users: {total_users_count}</pre>'
+        # Select a random image from the list using random.choice()
+        random_image = random.choice(photos)
 
-            # Select a random image from the list
-            random_image = random.choice(photos)
-
-            await message.reply_photo(
-                photo=random_image, 
-                caption=status_message,
-                message_effect_id=5104841245755180586  # ✅ Correctly placed
-            )
-
-            await sticker.delete()
-        else:
-            await message.reply_text("You are not an authorized user!")
-
+        await message.reply_photo(
+            photo=random_image, 
+            caption=status_message
+        )
+        await sticker.delete()
     else:
-        if message.from_user.id in get_admins() + SUDO_USERS:
-            sticker = await message.reply_sticker("CAACAgUAAxkBAAEG8_xnzbAQQOfHqMQrzWcOHBvU78EiRgAC_hMAAqiR-FZkMEjJt_CizDYE")
-
-            now = datetime.now()
-            delta = now - bot.uptime
-            time = get_readable_time(delta.seconds)
-            ping = await get_ping(bot)
-            total_users_count = len(await full_userbase())
-
-            status_message = f'<pre language="@RedHoodXbot Bot Status:">⏳ Bot Uptime: {time}\n⚡ Current Ping: {ping} ms\n👤 Total Users: {total_users_count}</pre>'
-
-            # Select a random image from the list
-            random_image = random.choice(photos)
-
-            await message.reply_photo(
-                photo=random_image, 
-                caption=status_message
-            )
-
-            await sticker.delete()
-        else:
-            await message.reply_text("You are not an authorized user!")
+        await message.reply_text("You are not an authorized user!")
