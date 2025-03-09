39e42ee8f8797e6ae56fd479fd6252767d931b9d
NotCodingbot <notcodingbot@gmail.com>
2025-03-10 04:58:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -204,8 +204,11 @@ async def send_text(client: Bot, message: Message):
             pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
             for chat_id in query:
                 try:
-                    await broadcast_msg.copy(chat_id)
+                    sent_msg = await broadcast_msg.copy(chat_id)
+                    
                     successful += 1
+
+                    
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
                     await broadcast_msg.copy(chat_id)
