bc07555506504416cdc68c363fa0c235fe4734de
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-03-19 00:40:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -127,6 +127,7 @@ async def start_command(client: Client, message: Message):
         return
     
 
+
 @Bot.on_message(filters.command('start'))
 async def not_joined(client: Client, message: Message):
     await message.react("👎")
@@ -146,10 +147,13 @@ async def not_joined(client: Client, message: Message):
     await refresh_force_sub_channels()
 
     buttons = []
+    tasks = []
     for i in range(1, 5):
-        invitelink = (await client.get_chat(get_force_sub_channel(i))).invite_link if get_force_sub_channel(i) else None
-        if invitelink:
-            buttons.append([InlineKeyboardButton(text=f"Join Channel {i}", url=invitelink)])
+        channel_id = get_force_sub_channel(i)
+        if channel_id:
+            tasks.append(check_channel_membership(client, channel_id, message.from_user.id, buttons))
+
+    await asyncio.gather(*tasks)
 
     try:
         buttons.append(
@@ -167,7 +171,7 @@ async def not_joined(client: Client, message: Message):
     await animation_message.delete()
 
     # Check if user has joined all force sub channels before sending force sub message
-    if not await check_force_sub(client, message.from_user.id):
+    if buttons:
         random_photo = random.choice(PHOTOS)
         await client.send_photo(
             chat_id=message.chat.id,
@@ -182,11 +186,19 @@ async def not_joined(client: Client, message: Message):
         )
         return
 
-    # Delete the animation message
-    
-
     # User has joined all channels, proceed with start command
     await start_command(client, message)
+
+async def check_channel_membership(client, channel_id, user_id, buttons):
+    try:
+        # Check if the user is already a member of the channel
+        member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
+        if member.status not in ['member', 'administrator', 'creator']:
+            chat = await client.get_chat(chat_id=channel_id)
+            invitelink = await client.create_chat_invite_link(chat_id=channel_id)
+            buttons.append([InlineKeyboardButton(text=chat.title, url=invitelink.invite_link)])
+    except Exception as e:
+        pass
 
 
 @Bot.on_message(filters.command('users') & filters.private)
