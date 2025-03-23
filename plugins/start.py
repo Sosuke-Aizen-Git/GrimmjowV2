84d93d46804957574f5851a994619cb076d626d6
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-03-23 23:13:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,7 +18,6 @@ import random  # Import random to select a random photo
 
 file_auto_delete = humanize.naturaldelta(get_auto_delete_time())
 
-
 @Bot.on_message(filters.command('start') & subscribed)
 async def start_command(client: Client, message: Message):
     Emoji = await message.reply("⏳")
@@ -32,6 +31,10 @@ async def start_command(client: Client, message: Message):
             await add_user(id)
         except:
             pass
+
+    # Save the command message URL
+    command_message_url = f"https://t.me/{client.username}?start={message.command[1]}"
+
     text = message.text
     if len(text) > 7:
         try:
@@ -94,10 +97,10 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
 
-        k = await client.send_message(chat_id=message.from_user.id, text=f"<blockquote><b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Video / File Will Be Deleted In {file_auto_delete} (Due To Copyright Issue Or Other Reasons).\n\n📌 Please Forward This Video / File To Somewhere Else And Start Downloading There.</blockquote>")
+        k = await client.send_message(chat_id=message.from_user.id, text=f"<blockquote><b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Video / File Will Be Deleted In {file_auto_delete} (Due To Copyrigh[...]")
 
         # Schedule the file deletion
-        asyncio.create_task(delete_files(madflix_msgs, client, k))
+        asyncio.create_task(delete_files(madflix_msgs, client, k, command_message_url))
 
         return
     else:
@@ -320,7 +323,7 @@ async def force_subs(client, message):
     await temp_mssg.edit(f"<blockquote>Here is the list of force subscription channels:</blockquote>\n\n{channel_info}", reply_markup=reply_markup)
 
 # Function to handle file deletion
-async def delete_files(messages, client, k):
+async def delete_files(messages, client, k, command_message_url):
     await asyncio.sleep(get_auto_delete_time())  # Wait for the duration specified in config.py
     for msg in messages:
         if msg is not None:  # Ensure msg is not None
@@ -328,8 +331,15 @@ async def delete_files(messages, client, k):
                 await client.delete_messages(chat_id=msg.chat.id, message_ids=[msg.id])
             except Exception as e:
                 print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
-    await k.edit_text("Your Video / File Is Successfully Deleted ✅")
 
+    # Create the inline keyboard with the "Try Again" button
+    inline_keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Try Again", url=command_message_url)]
+        ]
+    )
+
+    await k.edit_text("Your Video / File Is Successfully Deleted ✅", reply_markup=inline_keyboard)
      
 
 from pyrogram import Client, filters
