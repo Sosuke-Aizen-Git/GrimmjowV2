3a1e2e6d3c3f4723bb2305bfd0481415ff0a7a3d
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-03-23 18:02:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -126,7 +126,6 @@ async def start_command(client: Client, message: Message):
         )
         return
     
-get_files=f"https://t.me/{client.username}?start={message.command[1]}"
 
 @Bot.on_message(filters.command('start'))
 async def not_joined(client: Client, message: Message):
@@ -321,10 +320,6 @@ async def force_subs(client, message):
     await temp_mssg.edit(f"<blockquote>Here is the list of force subscription channels:</blockquote>\n\n{channel_info}", reply_markup=reply_markup)
 
 # Function to handle file deletion
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import logging
-
 async def delete_files(messages, client, k):
     await asyncio.sleep(get_auto_delete_time())  # Wait for the duration specified in config.py
     for msg in messages:
@@ -333,17 +328,8 @@ async def delete_files(messages, client, k):
                 await client.delete_messages(chat_id=msg.chat.id, message_ids=[msg.id])
             except Exception as e:
                 print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
-    await k.edit_text(
-        "Your Video / File Is Successfully Deleted ✅",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Get Files", url=f"get_files"),
-                    InlineKeyboardButton("❌ Close", callback_data="close")
-                ]
-            ]
-        )
-    )
+    await k.edit_text("Your Video / File Is Successfully Deleted ✅")
+
      
 
 from pyrogram import Client, filters
